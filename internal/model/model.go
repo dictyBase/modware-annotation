db59ab694e5ae6a34298b8a425909a4227c50249
Siddhartha Basu <biosidd@gmail.com>
2018-11-30 21:34:20 +0000

    feat: add model structure type for exachaning data through repository interface

---
+++ internal/model/model.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+
+	driver "github.com/arangodb/go-driver"
+)
+
+type AnnoDoc struct {
+	driver.DocumentMeta
+	Value         string    `json:"value"`
+	EditableValue string    `json:"editable_value"`
+	CreatedBy     string    `json:"created_by"`
+	EnrtyId       string    `json:"entry_id"`
+	Rank          int64     `json:"rank"`
+	IsObsolete    bool      `json:"is_obsolete"`
+	Version       int64     `json:"version"`
+	CreatedAt     time.Time `json:"created_at"`
+	Ontology      string    `json:"ontology,omitempty"`
+	Tag           string    `json:"tag,omitempty"`
+	CvtId         string    `json:"cvtid,omitempty"`
+	NotFound      bool
+}
