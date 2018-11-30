9866b031cc481a7dfb9458a852c63add4bab9e11
Siddhartha Basu <biosidd@gmail.com>
2018-11-30 21:36:00 +0000

    feat: add interface to publish message for annotation microservice

---
+++ internal/message/message.go
@@ -0,0 +1,13 @@
+package message
+
+import (
+	"github.com/dictyBase/go-genproto/dictybaseapis/annotation"
+)
+
+// Publisher manages publishing of message
+type Publisher interface {
+	// Publis publishes the annotation object using the given subject
+	Publish(subject string, ann *annotation.TaggedAnnotation) error
+	// Close closes the connection to the underlying messaging server
+	Close() error
+}
