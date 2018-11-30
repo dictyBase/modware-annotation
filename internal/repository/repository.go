14b4d1bf609740a854b621713d45f8d270a26828
Siddhartha Basu <biosidd@gmail.com>
2018-11-30 21:33:02 +0000

    feat: add repository interface for annotation data source

---
+++ internal/repository/repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"github.com/dictyBase/go-genproto/dictybaseapis/annotation"
+	"github.com/dictyBase/modware-annotation/internal/model"
+)
+
+// TaggedAnnotationRepository is an interface for accessing annotation
+// data from its data sources
+type TaggedAnnotationRepository interface {
+	GetAnnotationById(id string) (*model.AnnoDoc, error)
+	GetAnnotationByEntry(req *annotation.EntryAnnotationRequest) (*model.AnnoDoc, error)
+	AddAnnotation(na *annotation.NewTaggedAnnotation) (*model.AnnoDoc, error)
+	EditAnnotation(ua *annotation.TaggedAnnotationUpdate) (*model.AnnoDoc, error)
+	RemoveAnnotation(id string) error
+	ListAnnotations(cursor int64, limit int64) ([]*model.AnnoDoc, error)
+}
