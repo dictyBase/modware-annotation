96079f737d1fcb8acaf20325be2d6e384a99e85e
Siddhartha Basu <biosidd@gmail.com>
2019-04-24 20:06:11 -0500

    feat: add few error custom error type for handling list results

--- internal/repository/error.go
+++ internal/repository/error.go
@@ -26,8 +26,35 @@ func IsAnnotationNotFound(err error) bool {
 	}
 	return false
 }
+
 func IsGroupNotFound(err error) bool {
 	if _, ok := err.(*GroupNotFound); ok {
+		return true
+	}
+	return false
+}
+
+type AnnoListNotFound struct{}
+
+func (al *AnnoListNotFound) Error() string {
+	return "annotation list not found"
+}
+
+func IsAnnotationListNotFound(err error) bool {
+	if _, ok := err.(*AnnoListNotFound); ok {
+		return true
+	}
+	return false
+}
+
+type AnnoGroupListNotFound struct{}
+
+func (agl *AnnoGroupListNotFound) Error() string {
+	return "annotation group list not found"
+}
+
+func IsAnnotationGroupListNotFound(err error) bool {
+	if _, ok := err.(*AnnoGroupListNotFound); ok {
 		return true
 	}
 	return false
