433b5c33c757a4c57ba20869dab2f96631a5f092
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-09-24 18:27:48 -0500

    refactor: use the modified repository constructor method

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -11,8 +11,11 @@ import (
 	"github.com/dictyBase/aphgrpc"
 	manager "github.com/dictyBase/arangomanager"
 	"github.com/dictyBase/go-genproto/dictybaseapis/annotation"
+	ontoarango "github.com/dictyBase/go-obograph/storage/arangodb"
 	"github.com/dictyBase/modware-annotation/internal/app/service"
+	"github.com/dictyBase/modware-annotation/internal/message"
 	"github.com/dictyBase/modware-annotation/internal/message/nats"
+	"github.com/dictyBase/modware-annotation/internal/repository"
 	"github.com/dictyBase/modware-annotation/internal/repository/arangodb"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
@@ -24,25 +27,15 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+type serverParams struct {
+	repo repository.TaggedAnnotationRepository
+	msg  message.Publisher
+}
+
 func RunServer(c *cli.Context) error {
-	anrepo, err := arangodb.NewTaggedAnnotationRepo(
-		getConnParams(c), getCollParams(c),
-	)
+	sp, err := repoAndNatsConn(c)
 	if err != nil {
-		return cli.NewExitError(
-			fmt.Sprintf("cannot connect to arangodb annotation repository %s", err.Error()),
-			2,
-		)
-	}
-	ms, err := nats.NewPublisher(
-		c.String("nats-host"), c.String("nats-port"),
-		gnats.MaxReconnects(-1), gnats.ReconnectWait(2*time.Second),
-	)
-	if err != nil {
-		return cli.NewExitError(
-			fmt.Sprintf("cannot connect to messaging server %s", err.Error()),
-			2,
-		)
+		return cli.NewExitError(err.Error(), 2)
 	}
 	grpcS := grpc.NewServer(
 		grpc_middleware.WithUnaryServerChain(
@@ -52,8 +45,8 @@ func RunServer(c *cli.Context) error {
 	)
 	srv, err := service.NewAnnotationService(
 		&service.ServiceParams{
-			Repository: anrepo,
-			Publisher:  ms,
+			Repository: sp.repo,
+			Publisher:  sp.msg,
 			Group:      "groups",
 			Options:    getGrpcOpt(),
 		})
@@ -106,33 +99,29 @@ func getLogger(c *cli.Context) *logrus.Entry {
 	return logrus.NewEntry(log)
 }
 
-func getConnParams(c *cli.Context) *manager.ConnectParams {
+func allParams(c *cli.Context) (*manager.ConnectParams, *arangodb.CollectionParams, *ontoarango.CollectionParams) {
 	arPort, _ := strconv.Atoi(c.String("arangodb-port"))
 	return &manager.ConnectParams{
-		User:     c.String("arangodb-user"),
-		Pass:     c.String("arangodb-pass"),
-		Database: c.String("arangodb-database"),
-		Host:     c.String("arangodb-host"),
-		Port:     arPort,
-		Istls:    c.Bool("is-secure"),
-	}
-}
-
-func getCollParams(c *cli.Context) *arangodb.CollectionParams {
-	return &arangodb.CollectionParams{
-		Term:         c.String("term-collection"),
-		Relationship: c.String("rel-collection"),
-		GraphInfo:    c.String("cv-collection"),
-		OboGraph:     c.String("obograph"),
-		Annotation:   c.String("anno-collection"),
-		AnnoTerm:     c.String("annoterm-collection"),
-		AnnoVersion:  c.String("annover-collection"),
-		AnnoTagGraph: c.String("annoterm-graph"),
-		AnnoVerGraph: c.String("annover-graph"),
-		AnnoGroup:    c.String("annogroup-collection"),
-		AnnoIndexes:  c.StringSlice("annotation-index-fields"),
-		TermIndexes:  c.StringSlice("term-index-fields"),
-	}
+			User:     c.String("arangodb-user"),
+			Pass:     c.String("arangodb-pass"),
+			Database: c.String("arangodb-database"),
+			Host:     c.String("arangodb-host"),
+			Port:     arPort,
+			Istls:    c.Bool("is-secure"),
+		}, &arangodb.CollectionParams{
+			Annotation:   c.String("anno-collection"),
+			AnnoTerm:     c.String("annoterm-collection"),
+			AnnoVersion:  c.String("annover-collection"),
+			AnnoTagGraph: c.String("annoterm-graph"),
+			AnnoVerGraph: c.String("annover-graph"),
+			AnnoGroup:    c.String("annogroup-collection"),
+			AnnoIndexes:  c.StringSlice("annotation-index-fields"),
+		}, &ontoarango.CollectionParams{
+			GraphInfo:    c.String("cv-collection"),
+			OboGraph:     c.String("obograph"),
+			Relationship: c.String("rel-collection"),
+			Term:         c.String("term-collection"),
+		}
 }
 
 func getGrpcOpt() []aphgrpc.Option {
@@ -143,4 +132,24 @@ func getGrpcOpt() []aphgrpc.Option {
 			"annotationUpdate": "AnnotationService.Update",
 		}),
 	}
+}
+
+func repoAndNatsConn(c *cli.Context) (*serverParams, error) {
+	anrepo, err := arangodb.NewTaggedAnnotationRepo(allParams(c))
+	if err != nil {
+		return &serverParams{},
+			fmt.Errorf("cannot connect to arangodb annotation repository %s", err)
+	}
+	ms, err := nats.NewPublisher(
+		c.String("nats-host"), c.String("nats-port"),
+		gnats.MaxReconnects(-1), gnats.ReconnectWait(2*time.Second),
+	)
+	if err != nil {
+		return &serverParams{},
+			fmt.Errorf("cannot connect to messaging server %s", err)
+	}
+	return &serverParams{
+		repo: anrepo,
+		msg:  ms,
+	}, nil
 }
