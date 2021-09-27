c4f12f1541cf58fb740a5ec1e127f42db2aa68a6
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2021-09-27 13:33:00 -0500

    refactor: reorganize methods into different files according to their function

--- internal/repository/arangodb/ontology.go
+++ internal/repository/arangodb/ontology.go
@@ -56,3 +56,42 @@ func persistNewOboGraph(ds ontostorage.DataSource, g graph.OboGraph) (model.Uplo
 	}
 	return model.Created, nil
 }
+
+func (ar *arangorepository) termID(onto, term string) (string, error) {
+	var id string
+	r, err := ar.database.GetRow(annExistTagQ, map[string]interface{}{
+		"@cv_collection":     ar.onto.Cv.Name(),
+		"@cvterm_collection": ar.onto.Term.Name(),
+		"ontology":           onto,
+		"tag":                term,
+	})
+	if err != nil {
+		return id, fmt.Errorf("error in running obograph retrieving query %s", err)
+	}
+	if r.IsEmpty() {
+		return id, fmt.Errorf("ontology %s and tag %s does not exist", onto, term)
+	}
+	if err := r.Read(&id); err != nil {
+		return id, fmt.Errorf("error in retrieving obograph id %s", err)
+	}
+	return id, nil
+}
+
+func (ar *arangorepository) termName(id string) (string, error) {
+	var name string
+	cvtr, err := ar.database.GetRow(cvtID2LblQ, map[string]interface{}{
+		"@cvterm_collection": ar.onto.Term.Name(),
+		"id":                 id,
+	})
+	if err != nil {
+		return name,
+			fmt.Errorf("error in running tag retrieving query %s", err)
+	}
+	if cvtr.IsEmpty() {
+		return name, fmt.Errorf("cvterm id %s does not exist", id)
+	}
+	if err := cvtr.Read(&name); err != nil {
+		return name, fmt.Errorf("error in retrieving tag %s", err)
+	}
+	return name, nil
+}
