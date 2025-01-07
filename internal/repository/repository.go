dac9a9cdc4c1ea9631a0bf2fd624bf90d59c0f22
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-07 14:02:54 -0600

    refactor(repository.go): format method signatures for better readability by adding line breaks

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -14,13 +14,21 @@ import (
 type TaggedAnnotationRepository interface {
 	// GetAnnotationById retrieves an annotation
 	GetAnnotationByID(id string) (*model.AnnoDoc, error)
-	GetAnnotationByEntry(req *annotation.EntryAnnotationRequest) (*model.AnnoDoc, error)
+	GetAnnotationByEntry(
+		req *annotation.EntryAnnotationRequest,
+	) (*model.AnnoDoc, error)
 	AddAnnotation(na *annotation.NewTaggedAnnotation) (*model.AnnoDoc, error)
-	EditAnnotation(ua *annotation.TaggedAnnotationUpdate) (*model.AnnoDoc, error)
+	EditAnnotation(
+		ua *annotation.TaggedAnnotationUpdate,
+	) (*model.AnnoDoc, error)
 	RemoveAnnotation(id string, purge bool) error
 	// ListAnnotationGroup provides a paginated list of annotation along
 	// with optional filtering
-	ListAnnotations(cursor int64, limit int64, filter string) ([]*model.AnnoDoc, error)
+	ListAnnotations(
+		cursor int64,
+		limit int64,
+		filter string,
+	) ([]*model.AnnoDoc, error)
 	ClearAnnotations() error
 	Clear() error
 	// AddAnnotationGroup creates a new annotation group
@@ -28,14 +36,23 @@ type TaggedAnnotationRepository interface {
 	// GetAnnotationGroup retrieves an annotation group
 	GetAnnotationGroup(groupID string) (*model.AnnoGroup, error)
 	// AppendToAnnotationGroup adds new annotations to an existing group
-	AppendToAnnotationGroup(groupID string, idslice ...string) (*model.AnnoGroup, error)
+	AppendToAnnotationGroup(
+		groupID string,
+		idslice ...string,
+	) (*model.AnnoGroup, error)
 	// DeleteAnnotationGroup deletes an annotation group
 	RemoveAnnotationGroup(groupID string) error
 	// RemoveFromAnnotationGroup remove annotations from an existing group
-	RemoveFromAnnotationGroup(groupID string, idslice ...string) (*model.AnnoGroup, error)
+	RemoveFromAnnotationGroup(
+		groupID string,
+		idslice ...string,
+	) (*model.AnnoGroup, error)
 	// ListAnnotationGroup provides a paginated list of annotation groups along
 	// with optional filtering
-	ListAnnotationGroup(cursor, limit int64, filter string) ([]*model.AnnoGroup, error)
+	ListAnnotationGroup(
+		cursor, limit int64,
+		filter string,
+	) ([]*model.AnnoGroup, error)
 	// GetAnnotationTag retrieves tag information
 	GetAnnotationTag(name, ontology string) (*model.AnnoTag, error)
 	Dbh() *manager.Database
