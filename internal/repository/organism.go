1189a5bc958955125ef70817926f09b29411112d
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-07 14:56:10 -0600

    feat(repository): introduce OrganismRepository interface
    Define an interface for accessing organism data to improve data
    encapsulation and allow for easier switching between different data
    sources.

---
+++ internal/repository/organism.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	manager "github.com/dictyBase/arangomanager"
+	"github.com/dictyBase/modware-annotation/internal/model"
+)
+
+// OrganismRepository is an interface for accessing organism data
+// from its data sources.
+type OrganismRepository interface {
+	// GetOrganism retrieves an organism by ID
+	GetOrganism(id string) (*model.OrganismDoc, error)
+	// GetOrganismByName retrieves an organism by scientific name (genus + species)
+	GetOrganismByName(genus, species string) (*model.OrganismDoc, error)
+	// AddOrganism creates a new organism
+	AddOrganism(doc *model.OrganismDoc) (*model.OrganismDoc, error)
+	// EditOrganism updates an existing organism
+	EditOrganism(doc *model.OrganismDoc) (*model.OrganismDoc, error)
+	// RemoveOrganism deletes an organism
+	RemoveOrganism(id string) error
+	// ListOrganisms provides a paginated list of organisms along with optional filtering
+	ListOrganisms(
+		cursor int64,
+		limit int64,
+		filter string,
+	) ([]*model.OrganismDoc, error)
+	// ClearOrganisms removes all organisms
+	ClearOrganisms() error
+	// Dbh returns the underlying database handler
+	Dbh() *manager.Database
+}
