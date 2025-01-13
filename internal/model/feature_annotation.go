7ba67dd520413ab4f22972c2891e198c688e8033
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-13 13:31:05 -0600

    refactor: make Type and Name fields optional
    Allowing Type and Name fields to be optional improves flexibility in handling cases where these fields may not be present or required.

--- internal/model/feature_annotation.go
+++ internal/model/feature_annotation.go
@@ -17,13 +17,13 @@ type DbxrefDoc struct {
 // database.
 type FeatureAnnotationDoc struct {
 	driver.DocumentMeta
-	Type         string      `json:"feature_type"`
+	Type         string      `json:"feature_type,omitempty"`
 	Id           string      `json:"id"`
 	CreatedAt    time.Time   `json:"created_at"`
 	UpdatedAt    time.Time   `json:"updated_at"`
 	CreatedBy    string      `json:"created_by"`
 	UpdatedBy    string      `json:"updated_by"`
-	Name         string      `json:"name"`
+	Name         string      `json:"name,omitempty"`
 	Synonyms     []string    `json:"synonyms,omitempty"`
 	Publications []string    `json:"publications,omitempty"`
 	Pubmed       []string    `json:"pubmed,omitempty"`
