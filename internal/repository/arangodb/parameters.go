b4863843338d4c306acfce2495bc46377b9f8838
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-13 12:40:42 -0600

    feat: add FeatureCollectionParams struct
    Introduce a new struct to hold parameters for feature annotation
    collections, making the code more organized and maintainable.

--- internal/repository/arangodb/parameters.go
+++ internal/repository/arangodb/parameters.go
@@ -38,3 +38,9 @@ type CollectionParams struct {
 	// Annotation collection
 	AnnoIndexes []string `validate:"required"`
 }
+
+// FeatureCollectionParams contains the parameters for feature annotation collections
+type FeatureCollectionParams struct {
+	// Feature is the collection for storing feature annotations
+	Feature string `validate:"required"`
+}
