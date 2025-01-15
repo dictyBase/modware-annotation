5a9fc3bd77420fb3eae2865de2b850d03f2a610b
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-15 15:06:13 -0600

    refactor(arangodb): extract helper functions to separate file
    The code is being refactored to improve organization and maintainability
    by moving helper functions to a separate file, reducing the size and
    complexity of the main file.

--- internal/repository/arangodb/feature_annotation.go
+++ internal/repository/arangodb/feature_annotation.go
@@ -10,7 +10,6 @@ import (
 	"github.com/dictyBase/modware-annotation/internal/collection"
 	"github.com/dictyBase/modware-annotation/internal/model"
 	"github.com/dictyBase/modware-annotation/internal/repository"
-	"github.com/go-playground/validator/v10"
 )
 
 type featureAnnoRepo struct {
@@ -49,104 +48,15 @@ func NewFeatureAnnoRepo(
 	}, nil
 }
 
-func validateParams(collP *FeatureCollectionParams) error {
-	if err := validator.New().Struct(collP); err != nil {
-		return fmt.Errorf("invalid collection parameters: %w", err)
-	}
-
-	return nil
-}
-
-func createSession(
-	connP *manager.ConnectParams,
-) (*manager.Session, *manager.Database, error) {
-	sess, dbh, err := manager.NewSessionDb(connP)
-	if err != nil {
-		return nil, nil, fmt.Errorf(
-			"failed to create database session: %w",
-			err,
-		)
-	}
-
-	return sess, dbh, nil
-}
-
-func createFeatureCollection(
-	dbh *manager.Database,
-	collP *FeatureCollectionParams,
-) (driver.Collection, error) {
-	// Get JSON schema for validation
-	schema, err := model.FeatureAnnotationSchema()
-	if err != nil {
-		return nil, fmt.Errorf(
-			"failed to generate feature annotation schema: %w",
-			err,
-		)
-	}
-
-	schemaOpt := &driver.CollectionSchemaOptions{
-		Level:   driver.CollectionSchemaLevelModerate,
-		Message: "Feature annotation validation failed",
-		Type:    "json",
-	}
-	if err := schemaOpt.LoadRule(schema); err != nil {
-		return nil, fmt.Errorf("error in loading schema %s", err)
-	}
-	// Create collection with schema validation
-	coll, err := dbh.FindOrCreateCollection(
-		collP.Feature,
-		&driver.CreateCollectionOptions{
-			Schema: schemaOpt,
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf(
-			"failed to create/find feature collection: %w",
-			err,
-		)
-	}
-
-	return coll, nil
-}
-
-func createIndices(dbh *manager.Database, coll driver.Collection) error {
-	// Create compound index for id and version
-	_, _, err := dbh.EnsurePersistentIndex(
-		coll.Name(),
-		[]string{"id", "vesion"},
-		&driver.EnsurePersistentIndexOptions{
-			InBackground: true,
-			Unique:       true,
-		},
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create id-version index: %w", err)
-	}
-
-	// Create index for name field
-	_, _, err = dbh.EnsurePersistentIndex(
-		coll.Name(),
-		[]string{"name"},
-		&driver.EnsurePersistentIndexOptions{
-			InBackground: true,
-		},
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create name index: %w", err)
-	}
-
-	return nil
-}
-
 // GetFeatureAnnotation retrieves a feature annotation by ID.
-func (fr *featureAnnoRepo) GetFeatureAnnotation(
+func (fann *featureAnnoRepo) GetFeatureAnnotation(
 	fid string,
 ) (*model.FeatureAnnotationDoc, error) {
 	doc := &model.FeatureAnnotationDoc{}
-	res, err := fr.database.GetRow(
+	res, err := fann.database.GetRow(
 		featureGetByIdQ,
 		map[string]interface{}{
-			"@collection": fr.feature.Name(),
+			"@collection": fann.feature.Name(),
 			"id":          fid,
 		},
 	)
@@ -164,7 +74,7 @@ func (fr *featureAnnoRepo) GetFeatureAnnotation(
 }
 
 // AddFeatureAnnotation creates a new feature annotation.
-func (fr *featureAnnoRepo) AddFeatureAnnotation(
+func (fann *featureAnnoRepo) AddFeatureAnnotation(
 	doc *feature.NewFeatureAnnotation,
 ) (*model.FeatureAnnotationDoc, error) {
 	// Create new feature annotation document
@@ -187,7 +97,7 @@ func (fr *featureAnnoRepo) AddFeatureAnnotation(
 	}
 
 	// Insert document into collection
-	meta, err := fr.feature.CreateDocument(context.Background(), faDoc)
+	meta, err := fann.feature.CreateDocument(context.Background(), faDoc)
 	if err != nil {
 		return nil, fmt.Errorf(
 			"error creating feature annotation document: %w",
@@ -202,10 +112,10 @@ func (fr *featureAnnoRepo) AddFeatureAnnotation(
 }
 
 // EditFeatureAnnotation updates an existing feature annotation.
-func (fr *featureAnnoRepo) EditFeatureAnnotation(
+func (fann *featureAnnoRepo) EditFeatureAnnotation(
 	doc *feature.FeatureAnnotationUpdate,
 ) (*model.FeatureAnnotationDoc, error) {
-	faDoc, err := fr.GetFeatureAnnotation(doc.Id)
+	faDoc, err := fann.GetFeatureAnnotation(doc.Id)
 	if err != nil {
 		return nil, err
 	}
@@ -215,7 +125,11 @@ func (fr *featureAnnoRepo) EditFeatureAnnotation(
 		updateAttributes(faDoc, doc.Attributes)
 	}
 
-	meta, err := fr.feature.UpdateDocument(context.Background(), doc.Id, faDoc)
+	meta, err := fann.feature.UpdateDocument(
+		context.Background(),
+		doc.Id,
+		faDoc,
+	)
 	if err != nil {
 		return nil, fmt.Errorf("error in updating document %s", err)
 	}
@@ -224,70 +138,14 @@ func (fr *featureAnnoRepo) EditFeatureAnnotation(
 	return faDoc, nil
 }
 
-func updateBasicFields(
-	faDoc *model.FeatureAnnotationDoc,
-	doc *feature.FeatureAnnotationUpdate,
-) {
-	faDoc.UpdatedBy = doc.UpdatedBy
-	if faDoc.IsObsolete != doc.IsObsolete {
-		faDoc.IsObsolete = doc.IsObsolete
-	}
-}
-
-func updateAttributes(
-	mdoc *model.FeatureAnnotationDoc,
-	attrs *feature.FeatureAnnotationAttributes,
-) {
-	if len(attrs.Name) > 0 {
-		mdoc.Name = attrs.Name
-	}
-	if len(attrs.Synonyms) > 0 {
-		mdoc.Synonyms = append(mdoc.Synonyms, attrs.Synonyms...)
-	}
-	if len(attrs.Publications) > 0 {
-		mdoc.Publications = append(mdoc.Publications, attrs.Publications...)
-	}
-	if len(attrs.Pubmed) > 0 {
-		mdoc.Pubmed = append(mdoc.Pubmed, attrs.Pubmed...)
-	}
-	if len(attrs.Dblinks) > 0 {
-		mdoc.DbLinks = append(
-			mdoc.DbLinks,
-			collection.Map(attrs.Dblinks, convertDbLink)...)
-	}
-	if len(attrs.Properties) > 0 {
-		mdoc.Properties = append(
-			mdoc.Properties,
-			collection.Map(attrs.Properties, convertProperty)...)
-	}
-}
-
-func convertDbLink(link *feature.DbLink) model.DbLinkDoc {
-	return model.DbLinkDoc{
-		PrimaryId: link.PrimaryId,
-		Database:  link.Database,
-		Version:   link.Version,
-		LinkType:  link.Linktype,
-		URL:       link.Url,
-		Label:     link.Label,
-	}
-}
-
-func convertProperty(prop *feature.TagProperty) model.TagPropertyDoc {
-	return model.TagPropertyDoc{
-		Tag:   prop.Tag,
-		Value: prop.Value,
-	}
-}
-
 // ListFeatureAnnotations lists all feature annotations.
-func (fr *featureAnnoRepo) ListFeatureAnnotations() ([]*model.FeatureAnnotationDoc, error) {
+func (fann *featureAnnoRepo) ListFeatureAnnotations() ([]*model.FeatureAnnotationDoc, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
 // RemoveFeatureAnnotation deletes a feature annotation.
-func (fr *featureAnnoRepo) RemoveFeatureAnnotation(fid string) error {
-	_, err := fr.feature.RemoveDocument(context.Background(), fid)
+func (fann *featureAnnoRepo) RemoveFeatureAnnotation(fid string) error {
+	_, err := fann.feature.RemoveDocument(context.Background(), fid)
 	if err != nil {
 		if driver.IsNotFoundGeneral(err) {
 			return &repository.AnnoNotFoundError{Id: fid}
@@ -304,8 +162,8 @@ func (fr *featureAnnoRepo) RemoveFeatureAnnotation(fid string) error {
 }
 
 // ClearFeatureAnnotations removes all feature annotations.
-func (fr *featureAnnoRepo) ClearFeatureAnnotations() error {
-	if err := fr.feature.Truncate(context.Background()); err != nil {
+func (fann *featureAnnoRepo) ClearFeatureAnnotations() error {
+	if err := fann.feature.Truncate(context.Background()); err != nil {
 		return fmt.Errorf(
 			"error clearing feature annotations collection: %w",
 			err,
@@ -316,53 +174,7 @@ func (fr *featureAnnoRepo) ClearFeatureAnnotations() error {
 }
 
 // Dbh returns the underlying database handler.
-func setOptionalFields(
-	doc *feature.NewFeatureAnnotation,
-	faDoc *model.FeatureAnnotationDoc,
-) {
-	if len(doc.Attributes.Synonyms) > 0 {
-		faDoc.Synonyms = doc.Attributes.Synonyms
-	}
-	if len(doc.Attributes.Publications) > 0 {
-		faDoc.Publications = doc.Attributes.Publications
-	}
-	if len(doc.Attributes.Pubmed) > 0 {
-		faDoc.Pubmed = doc.Attributes.Pubmed
-	}
-	if len(doc.Attributes.Properties) > 0 {
-		faDoc.Properties = collection.Map(
-			doc.Attributes.Properties,
-			func(prop *feature.TagProperty) model.TagPropertyDoc {
-				return model.TagPropertyDoc{
-					Tag:   prop.Tag,
-					Value: prop.Value,
-				}
-			},
-		)
-	}
-}
 
-func toDbLink(link *feature.DbLink) model.DbLinkDoc {
-	dbLink := model.DbLinkDoc{
-		PrimaryId: link.PrimaryId,
-		Version:   link.Version,
-		Database:  link.Database,
-	}
-
-	// Set optional fields only if present
-	if link.Linktype != "" {
-		dbLink.LinkType = link.Linktype
-	}
-	if link.Url != "" {
-		dbLink.URL = link.Url
-	}
-	if link.Label != "" {
-		dbLink.Label = link.Label
-	}
-
-	return dbLink
-}
-
-func (fr *featureAnnoRepo) Dbh() *manager.Database {
-	return fr.database
+func (fann *featureAnnoRepo) Dbh() *manager.Database {
+	return fann.database
 }
---
+++ internal/repository/arangodb/feature_annotation_helpers.go
@@ -0,0 +1,199 @@
+package arangodb
+
+import (
+	"fmt"
+
+	driver "github.com/arangodb/go-driver"
+	manager "github.com/dictyBase/arangomanager"
+	feature "github.com/dictyBase/go-genproto/dictybaseapis/feature_annotation"
+	"github.com/dictyBase/modware-annotation/internal/collection"
+	"github.com/dictyBase/modware-annotation/internal/model"
+	"github.com/go-playground/validator/v10"
+)
+
+func validateParams(collP *FeatureCollectionParams) error {
+	if err := validator.New().Struct(collP); err != nil {
+		return fmt.Errorf("invalid collection parameters: %w", err)
+	}
+
+	return nil
+}
+
+func createSession(
+	connP *manager.ConnectParams,
+) (*manager.Session, *manager.Database, error) {
+	sess, dbh, err := manager.NewSessionDb(connP)
+	if err != nil {
+		return nil, nil, fmt.Errorf(
+			"failed to create database session: %w",
+			err,
+		)
+	}
+
+	return sess, dbh, nil
+}
+
+func createFeatureCollection(
+	dbh *manager.Database,
+	collP *FeatureCollectionParams,
+) (driver.Collection, error) {
+	schema, err := model.FeatureAnnotationSchema()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to generate feature annotation schema: %w",
+			err,
+		)
+	}
+
+	schemaOpt := &driver.CollectionSchemaOptions{
+		Level:   driver.CollectionSchemaLevelModerate,
+		Message: "Feature annotation validation failed",
+		Type:    "json",
+	}
+	if err := schemaOpt.LoadRule(schema); err != nil {
+		return nil, fmt.Errorf("error in loading schema %s", err)
+	}
+	coll, err := dbh.FindOrCreateCollection(
+		collP.Feature,
+		&driver.CreateCollectionOptions{
+			Schema: schemaOpt,
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to create/find feature collection: %w",
+			err,
+		)
+	}
+
+	return coll, nil
+}
+
+func createIndices(dbh *manager.Database, coll driver.Collection) error {
+	_, _, err := dbh.EnsurePersistentIndex(
+		coll.Name(),
+		[]string{"id", "vesion"},
+		&driver.EnsurePersistentIndexOptions{
+			InBackground: true,
+			Unique:       true,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create id-version index: %w", err)
+	}
+
+	_, _, err = dbh.EnsurePersistentIndex(
+		coll.Name(),
+		[]string{"name"},
+		&driver.EnsurePersistentIndexOptions{
+			InBackground: true,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create name index: %w", err)
+	}
+
+	return nil
+}
+
+func updateBasicFields(
+	faDoc *model.FeatureAnnotationDoc,
+	doc *feature.FeatureAnnotationUpdate,
+) {
+	faDoc.UpdatedBy = doc.UpdatedBy
+	if faDoc.IsObsolete != doc.IsObsolete {
+		faDoc.IsObsolete = doc.IsObsolete
+	}
+}
+
+func updateAttributes(
+	mdoc *model.FeatureAnnotationDoc,
+	attrs *feature.FeatureAnnotationAttributes,
+) {
+	if len(attrs.Name) > 0 {
+		mdoc.Name = attrs.Name
+	}
+	if len(attrs.Synonyms) > 0 {
+		mdoc.Synonyms = append(mdoc.Synonyms, attrs.Synonyms...)
+	}
+	if len(attrs.Publications) > 0 {
+		mdoc.Publications = append(mdoc.Publications, attrs.Publications...)
+	}
+	if len(attrs.Pubmed) > 0 {
+		mdoc.Pubmed = append(mdoc.Pubmed, attrs.Pubmed...)
+	}
+	if len(attrs.Dblinks) > 0 {
+		mdoc.DbLinks = append(
+			mdoc.DbLinks,
+			collection.Map(attrs.Dblinks, convertDbLink)...)
+	}
+	if len(attrs.Properties) > 0 {
+		mdoc.Properties = append(
+			mdoc.Properties,
+			collection.Map(attrs.Properties, convertProperty)...)
+	}
+}
+
+func convertDbLink(link *feature.DbLink) model.DbLinkDoc {
+	return model.DbLinkDoc{
+		PrimaryId: link.PrimaryId,
+		Database:  link.Database,
+		Version:   link.Version,
+		LinkType:  link.Linktype,
+		URL:       link.Url,
+		Label:     link.Label,
+	}
+}
+
+func convertProperty(prop *feature.TagProperty) model.TagPropertyDoc {
+	return model.TagPropertyDoc{
+		Tag:   prop.Tag,
+		Value: prop.Value,
+	}
+}
+
+func setOptionalFields(
+	doc *feature.NewFeatureAnnotation,
+	faDoc *model.FeatureAnnotationDoc,
+) {
+	if len(doc.Attributes.Synonyms) > 0 {
+		faDoc.Synonyms = doc.Attributes.Synonyms
+	}
+	if len(doc.Attributes.Publications) > 0 {
+		faDoc.Publications = doc.Attributes.Publications
+	}
+	if len(doc.Attributes.Pubmed) > 0 {
+		faDoc.Pubmed = doc.Attributes.Pubmed
+	}
+	if len(doc.Attributes.Properties) > 0 {
+		faDoc.Properties = collection.Map(
+			doc.Attributes.Properties,
+			func(prop *feature.TagProperty) model.TagPropertyDoc {
+				return model.TagPropertyDoc{
+					Tag:   prop.Tag,
+					Value: prop.Value,
+				}
+			},
+		)
+	}
+}
+
+func toDbLink(link *feature.DbLink) model.DbLinkDoc {
+	dbLink := model.DbLinkDoc{
+		PrimaryId: link.PrimaryId,
+		Version:   link.Version,
+		Database:  link.Database,
+	}
+
+	if link.Linktype != "" {
+		dbLink.LinkType = link.Linktype
+	}
+	if link.Url != "" {
+		dbLink.URL = link.Url
+	}
+	if link.Label != "" {
+		dbLink.Label = link.Label
+	}
+
+	return dbLink
+}
