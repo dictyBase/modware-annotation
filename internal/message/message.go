71cf1cfea5443dba5cdfd636ed0d66a9465b41ec
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-22 14:57:39 -0600

    feat(message.go): add FeatureAnnotationPublisher interface
    The new interface allows for publishing of feature annotation messages,
    increasing the functionality of the message package.

--- internal/message/message.go
+++ internal/message/message.go
@@ -2,12 +2,21 @@ package message
 
 import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/annotation"
+	feature "github.com/dictyBase/go-genproto/dictybaseapis/feature_annotation"
 )
 
 // Publisher manages publishing of message.
 type Publisher interface {
 	// Publis publishes the annotation object using the given subject
 	Publish(subject string, ann *annotation.TaggedAnnotation) error
+	// Close closes the connection to the underlying messaging server
+	Close() error
+}
+
+// FeatureAnnotationPublisher manages publishing of feature annotation messages.
+type FeatureAnnotationPublisher interface {
+	// Publish publishes the feature annotation object using the given subject
+	Publish(subject string, ann *feature.FeatureAnnotation) error
 	// Close closes the connection to the underlying messaging server
 	Close() error
 }
