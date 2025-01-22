399a8d2c30a5a83d119803c2fe885464f5f75eee
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-22 15:10:19 -0600

    feat: add NATS feature annotation publisher
    Implementing a publisher for feature annotations allows the application to send messages to a NATS server, enabling communication with other services that rely on feature annotations.

---
+++ internal/message/nats/feature_annotation.go
@@ -0,0 +1,53 @@
+//nolint:dupl
+package nats
+
+import (
+	"encoding/json"
+	"fmt"
+
+	feature "github.com/dictyBase/go-genproto/dictybaseapis/feature_annotation"
+	"github.com/dictyBase/modware-annotation/internal/message"
+	gnats "github.com/nats-io/nats.go"
+)
+
+type featureAnnotationPublisher struct {
+	conn *gnats.Conn
+}
+
+func NewFeatureAnnotationPublisher(
+	host, port string,
+	options ...gnats.Option,
+) (message.FeatureAnnotationPublisher, error) {
+	nconn, err := gnats.Connect(
+		fmt.Sprintf("nats://%s:%s", host, port),
+		options...)
+	if err != nil {
+		return &featureAnnotationPublisher{}, fmt.Errorf(
+			"error in connecting to nats server %s",
+			err,
+		)
+	}
+
+	return &featureAnnotationPublisher{conn: nconn}, nil
+}
+
+func (fnp *featureAnnotationPublisher) Publish(
+	subj string,
+	fann *feature.FeatureAnnotation,
+) error {
+	data, err := json.Marshal(fann)
+	if err != nil {
+		return fmt.Errorf("error in marshaling feature annotation %s", err)
+	}
+	if err := fnp.conn.Publish(subj, data); err != nil {
+		return fmt.Errorf("error in publishing through nats %s", err)
+	}
+
+	return nil
+}
+
+func (fnp *featureAnnotationPublisher) Close() error {
+	fnp.conn.Close()
+
+	return nil
+}
