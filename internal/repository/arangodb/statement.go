99c0bccb89b2fec42d2e73947c76680273369211
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-01-24 17:48:35 -0600

    feat(arangodb): add featurePurgeQ and featureObsoleteQ queries
    Implement queries to purge and mark features as obsolete, enhancing data management capabilities.

--- internal/repository/arangodb/statement.go
+++ internal/repository/arangodb/statement.go
@@ -6,6 +6,18 @@ const (
 			FILTER f.feature_id == @id
 			LIMIT 1
 			RETURN f._key
+	`
+	featurePurgeQ = `
+        FOR f IN @@collection
+            FILTER f.feature_id == @id
+            LIMIT 1
+            REMOVE f IN @@collection
+    `
+	featureObsoleteQ = `
+        FOR f IN @@collection
+            FILTER f.feature_id == @id
+            UPDATE f WITH { is_obsolete: true } IN @@collection
+    `
 	featureGetByIdQ = `FOR f IN @@collection 
     		FILTER f.feature_id == @id 
     		FILTER f.is_obsolete == false 
