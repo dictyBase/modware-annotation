60f41d174ceb95886fb60493f540807ab283fcd5
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-03-13 20:50:35 -0500

    feat(annotation_statement.go): add functions to generate AQL statements based on filter sets
    The new functions generate AQL statements for different filter
    combinations. This allows for more flexible querying of annotations
    based on various filter criteria. The functions handle cases where both,
    only the first, or only the second filter set is provided, ensuring
    comprehensive statement generation.

--- internal/repository/arangodb/annotation_statement.go
+++ internal/repository/arangodb/annotation_statement.go
@@ -4,3 +4,69 @@ type PickStatementResult struct {
 	Statement string
 	Err       error
 }
+// generateStatementForBothFilters creates a statement when both filter sets are
+// non-empty.
+func generateStatementForBothFilters[A, B []*query.Filter](
+	first A,
+	second B,
+) PickStatementResult {
+	var result PickStatementResult
+	fmap := FilterMap()
+
+	afilter, err := query.GenQualifiedAQLFilterStatement(fmap, first)
+	if err != nil {
+		result.Err = err
+
+		return result
+	}
+
+	cfilter, err := query.GenQualifiedAQLFilterStatement(fmap, second)
+	if err != nil {
+		result.Err = err
+
+		return result
+	}
+	result.Statement = fmt.Sprintf(annCvtListFilterQ, afilter, cfilter)
+
+	return result
+}
+
+// generateStatementForFirstFilter creates a statement when only first filter
+// set is non-empty.
+func generateStatementForFirstFilter[A []*query.Filter](
+	first A,
+) PickStatementResult {
+	var result PickStatementResult
+	fmap := FilterMap()
+
+	afilter, err := query.GenQualifiedAQLFilterStatement(fmap, first)
+	if err != nil {
+		result.Err = err
+
+		return result
+	}
+
+	result.Statement = fmt.Sprintf(annExclusiveListFilterQ, afilter)
+
+	return result
+}
+
+// generateStatementForSecondFilter creates a statement when only second filter
+// set is non-empty.
+func generateStatementForSecondFilter[B []*query.Filter](
+	second B,
+) PickStatementResult {
+	var result PickStatementResult
+	fmap := FilterMap()
+
+	cfilter, err := query.GenQualifiedAQLFilterStatement(fmap, second)
+	if err != nil {
+		result.Err = err
+
+		return result
+	}
+
+	result.Statement = fmt.Sprintf(cvtExclusiveListFilterQ, cfilter)
+
+	return result
+}
