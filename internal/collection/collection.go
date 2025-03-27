7067f3b302e15c73442283e791e961a0defa76b6
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2025-03-27 08:57:52 -0500

    feat(collection): add Pipe4 function to enable functional pipelines with four functions
    The Pipe4 function is added to the collection package to extend the functional pipeline capabilities. This allows developers to chain together four functions, enhancing code readability and enabling more complex data transformations in a functional style.

--- internal/collection/collection.go
+++ internal/collection/collection.go
@@ -163,6 +163,20 @@ func Pipe3[T1, T2, T3, T4 any](
 	return f3(f2(f1(initial)))
 }
 
+// Pipe4 creates a functional pipeline by taking an initial value and applying
+// four functions in succession. The output of each function becomes the input
+// to the next function. The final return value is the result of the last
+// function application.
+func Pipe4[T1, T2, T3, T4, T5 any](
+	initial T1,
+	f1 func(T1) T2,
+	f2 func(T2) T3,
+	f3 func(T3) T4,
+	f4 func(T4) T5,
+) T5 {
+	return f4(f3(f2(f1(initial))))
+}
+
 // Tuple2 represents a pair of values with independent types.
 // It's useful for functions that need to return two values of different types.
 type Tuple2[T1, T2 any] struct {
